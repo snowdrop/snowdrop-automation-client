07c132fca9c5115563806b41b05d0e5921d9f30e
Gytis Trikleris <gytis@redhat.com>
2020-03-20 19:50:15 +0100

    Reimplement UpdateBomFromUpstream command #47

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,7 +2,8 @@ export const SNOWDROP_ORG = "snowdrop";
 
 export const BOOSTER_GITHUB_TOPIC = "example";
 export const BOOSTER_BOM_PROPERTY_NAME = "spring-boot-bom.version";
-export const BOOSTER_SB_PROPERTY_NAME = "spring-boot.version";
+export const BOOSTER_SB_PROPERTY_NAME = "spring-boot.version"; // TODO use SPRING_BOOT_VERSION_PROPERTY_NAME
+export const SPRING_BOOT_VERSION_PROPERTY_NAME = "spring-boot.version";
 
 export const BOM_REPO = "spring-boot-bom";
 export const BOOSTER_CATALOG_REPO = "launcher-booster-catalog";
@@ -13,6 +14,6 @@ export const REDHAT_QUALIFIER = "redhat";
 export const BOOSTER_VERSION_REGEX = /^(\d+.\d+.\d+)-(\d+)(-\w+)?$/;
 export const BOOSTER_NAME_REGEX = /^(.+)-example$/;
 
-export const UPSTREAM_RELEASE_VERSION_REGEX = /^(\d+.\d+.\d+).RELEASE$/;
+export const SPRING_BOOT_VERSION_REGEX = /^(\d+.\d+.\d+).RELEASE$/;
 
 export const LICENSES_GENERATOR_PATH = "etc/licenses-generator-shaded.jar";
