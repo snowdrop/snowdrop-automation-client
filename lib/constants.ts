53a7552b9c2ed91436f74193715375115965ccf9
Gytis Trikleris <gytis@redhat.com>
2020-03-24 21:29:39 +0100

    Remove unused constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,8 +1,6 @@
 export const SNOWDROP_ORG = "snowdrop";
 
 export const BOOSTER_GITHUB_TOPIC = "example";
-export const BOOSTER_BOM_PROPERTY_NAME = "spring-boot-bom.version";
-export const BOOSTER_SB_PROPERTY_NAME = "spring-boot.version"; // TODO use SPRING_BOOT_VERSION_PROPERTY_NAME
 export const SPRING_BOOT_VERSION_PROPERTY_NAME = "spring-boot.version";
 
 export const BOM_REPO = "spring-boot-bom";
